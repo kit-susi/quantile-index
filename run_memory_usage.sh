c02217cce5d2ac580de13cdcc44196ced73e7668
jlabeit <julianlabeit@gmail.com>
2016-08-07 22:09:20 +0200

    Fixed script.

--- run_memory_usage.sh
+++ run_memory_usage.sh
@@ -7,7 +7,7 @@ do
 		./build/surf_index-IDX_$IDX -c collections/$COL -m m | tail -1 
 	done
 	echo $COL"INT"
-	./build/surf_index-IDX_NN -c collections/$COL"INT" -m m | tail -2 
+	./build/surf_index-IDX_NN_INT -c collections/$COL"INT" -m m | tail -2 
 	for IDX in O_SD_INT O_HYB_INT O_INT
 	do
 		./build/surf_index-IDX_$IDX -c collections/$COL"INT" -m m | tail -1 

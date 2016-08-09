af4735dd62b7e73a2a4aaf5b78f6acccaa40168d
jlabeit <julianlabeit@gmail.com>
2016-08-09 21:39:40 +0200

    Fixed script.

--- run_all.sh
+++ run_all.sh
@@ -2,10 +2,14 @@
 sh run_memory_usage.sh >> result_mem_usage.txt
 # Experiment 2
 cd benchmark
+# A
 rm results/*
+echo "10;5;1;0;0" >> experiment2.config
 make experiment2
 mv results/experiment2.txt experiment2a.txt
+# B
 rm results/*
+echo "10;5;1;140;30" >> experiment2.config
 make experiment2
 mv results/experiment2.txt experiment2b.txt
 

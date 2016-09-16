4fdef3c35ba61839fced6dd671d5f2489d2051fa
Niklas Baumstark <niklas.baumstark@gmail.com>
2016-09-16 21:13:55 +0200

    remove old update-sdsl.sh

--- update-sdsl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd external/sdsl-lite && \
-git checkout k2_algos && \
-git pull && \
-cd ../.. && \
-git add external/sdsl-lite && \
-git commit -m "forwarded sdsl-lite to current master"

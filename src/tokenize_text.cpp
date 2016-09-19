ca7fbda19c2d3cc780466266464ed3cd0d63c871
Niklas Baumstark <niklas.baumstark@gmail.com>
2016-09-19 15:10:32 +0200

    modify tokenize_text to allow for input to be an SDSL 8-bit int vector

--- src/tokenize_text.cpp
+++ src/tokenize_text.cpp
@@ -23,7 +23,8 @@ string space() {
         if (std::isspace(c))
             res += c;
     }
-    return res + "\1";
+    res += '\1';
+    return res;
 }
 
 string punct() {
@@ -36,12 +37,41 @@ string punct() {
     return res;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "Usage: " << argv[0] << " file_name" << endl;
-        cout << " Takes a text file containing documents seperated with 1 (binary)." << endl;
-        cout << " Stores parsed collection in `text_int_SURF.sdsl` and `dict.txt`" << endl;
+string outdir = ".";
+string infile;
+bool input_is_sdsl = false;
+
+void usage(string prog) {
+    cout << "Usage: " << prog << " [-o outdir] [-s] file_name" << endl;
+    cout << "  Takes a text file containing documents seperated with 1 (binary)." << endl;
+    cout << "  Stores parsed collection in `text_int_SURF.sdsl` and `dict.txt`." << endl;
+    cout << "  If -s is given, input is assumed to be a text surf collection " << endl;
+    cout << "  (i.e. a text_SURF.sdsl file)" << endl;
+    exit(EXIT_FAILURE);
+}
+
+void parse_opts(int argc, char* argv[]) {
+    int op;
+    while ((op = getopt(argc, argv, "o:s")) != -1) {
+        switch (op) {
+            case 'o':
+                outdir = optarg;
+                break;
+            case 's':
+                input_is_sdsl = true;
+                break;
+            case '?':
+            default:
+                usage(argv[0]);
+        }
     }
+    if (argc - optind != 1) usage(argv[0]);
+    infile = argv[optind];
+}
+
+int main(int argc, char* argv[]) {
+    parse_opts(argc, argv);
+
     //int_vector<> text;
     //load_from_file(text, argv[1]);
     //text.resize(text.size()+1);
@@ -51,10 +81,15 @@ int main(int argc, char* argv[]) {
     std::istreambuf_iterator<char> > tokenizer;
     boost::char_separator<char> sep(punct().c_str(), space().c_str());
     unordered_map<string, uint64_t> dictionary;
-    uint64_t num_docs = 1;
+    uint64_t num_docs = 0;
     uint64_t total_size = 0;
     {   // Build dictionary.
-        ifstream ifile(argv[1]);
+        ifstream ifile(infile);
+        if (input_is_sdsl) {
+            // skip int vector header
+            char throwaway[8];
+            ifile.read(throwaway, 8);
+        }
         istreambuf_iterator<char> file_iter(ifile);
         istreambuf_iterator<char> end_of_stream;
         tokenizer tokens(file_iter, end_of_stream, sep);
@@ -79,7 +114,7 @@ int main(int argc, char* argv[]) {
     text[total_size] = 0;
     vector<uint64_t> word_count(dictionary.size(), 0);
     {   // Count words and built integer sequence.
-        ifstream ifile(argv[1]);
+        ifstream ifile(infile);
         istreambuf_iterator<char> file_iter(ifile);
         istreambuf_iterator<char> end_of_stream;
         tokenizer tokens(file_iter, end_of_stream, sep);
@@ -99,14 +134,14 @@ int main(int argc, char* argv[]) {
     }
     util::bit_compress(text);
     //for (const auto& w : text) cout << w << " "; cout << endl;
-    store_to_file(text, "text_int_SURF.sdsl");
+    store_to_file(text, outdir + "/text_int_SURF.sdsl");
     // Sort dict.
     vector<pair<string, int>> dict(dictionary.begin(), dictionary.end());
     sort(dict.begin(), dict.end(),  [&](const auto & a, const auto & b) {
         return word_count[a.second] > word_count[b.second];
     });
     {   // Output dictionary.
-        fstream fout("dict.txt", ios::out);
+        fstream fout(outdir + "/dict.txt", ios::out);
         for (const auto& entry : dict) {
             fout << entry.first << " " << entry.second + 2 << " " <<
                  word_count[entry.second] << endl;

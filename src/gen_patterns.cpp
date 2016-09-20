7c8ab42cf5b6487b6357859dfdc63a47cd4e03fd
kittobi92 <Tobias.Heuer@gmx.net>
2016-09-20 11:10:48 +0200

    format gen_patterns

--- src/gen_patterns.cpp
+++ src/gen_patterns.cpp
@@ -25,7 +25,7 @@ typedef struct cmdargs {
 void
 usage(char* program)
 {
-    printf("%s -c <collection directory> -m\n",program);
+    printf("%s -c <collection directory> -m\n", program);
     printf("where\n");
     printf("  -c <collection directory>  : the directory the collection is stored.\n");
     printf("  -m <pattern length>        : the  pattern length.\n");
@@ -37,14 +37,14 @@ usage(char* program)
 };
 
 cmdargs_t
-parse_args(int argc,char* const argv[])
+parse_args(int argc, char* const argv[])
 {
     cmdargs_t args;
     int op;
     args.collection_dir = "";
     args.pat_len = 0;
     args.pat_cnt = 0;
-    args.ngram_samples = 1<<13;
+    args.ngram_samples = 1 << 13;
     args.min_sampling = 0;
     args.int_collection = false;
 
@@ -52,7 +52,7 @@ parse_args(int argc,char* const argv[])
     srand(chrono::duration_cast<chrono::microseconds>(now.time_since_epoch()).count());
     args.seed = rand();
 
-    while ((op=getopt(argc,argv,"ic:m:x:o:s:n:")) != -1) {
+    while ((op = getopt(argc, argv, "ic:m:x:o:s:n:")) != -1) {
         switch (op) {
             case 'i':
                 args.int_collection = true;
@@ -80,10 +80,10 @@ parse_args(int argc,char* const argv[])
                 usage(argv[0]);
         }
     }
-    if (args.collection_dir=="" || args.pat_len==0 || args.pat_cnt==0) {
+    if (args.collection_dir == "" || args.pat_len == 0 || args.pat_cnt == 0) {
         std::cerr << "Missing command line parameters.\n";
         usage(argv[0]);
-    } else if(args.ngram_samples < args.min_sampling) {
+    } else if (args.ngram_samples < args.min_sampling) {
         std::cerr << "o should be greater then n.\n";
         usage(argv[0]);
     }
@@ -92,12 +92,12 @@ parse_args(int argc,char* const argv[])
 
 template <typename T, typename R, typename S>
 void get_ngram(T& buf, R& dice, size_t n, S& res) {
-    for(;;) {
+    for (;;) {
         auto x = dice();
         bool valid = true;
-        for (size_t j=x; j < x + n and valid; ++j){
-            res[j-x] = buf[j];
-            if ( buf[j] == 0 or buf[j] == 1 or buf[j] == '\n' or buf[j] == '\r')
+        for (size_t j = x; j < x + n and valid; ++j) {
+            res[j - x] = buf[j];
+            if (buf[j] == 0 or buf[j] == 1 or buf[j] == '\n' or buf[j] == '\r')
                 valid = false;
         }
         if (valid) return;
@@ -106,91 +106,91 @@ void get_ngram(T& buf, R& dice, size_t n, S& res) {
 
 class string_vector_hash_fn {
 public:
-	std::size_t operator()(std::vector<int> const& vec) const {
-		std::size_t ret = 0;
-		for(auto& i : vec) {
-			ret ^= std::hash<uint32_t>()(i);
-		}
-		return ret;
-	}
-	
-	std::size_t operator()(std::string const& s) const {
-		std::hash<string> h;
-		return h(s);
-	}
+    std::size_t operator()(std::vector<int> const& vec) const {
+        std::size_t ret = 0;
+        for (auto & i : vec) {
+            ret ^= std::hash<uint32_t>()(i);
+        }
+        return ret;
+    }
+
+    std::size_t operator()(std::string const& s) const {
+        std::hash<string> h;
+        return h(s);
+    }
 };
 
 
 template<typename S, typename T>
 int find_ngrams(T& text_buf, cmdargs_t& args) {
-	if ( text_buf.size() < args.pat_len ){
-		std::cout<<"ERROR: text.size()="<<text_buf.size()<<" < m=" <<args.pat_len << std::endl;
-		return 1;
-	}
-	
-	//cout << "seed = " << args.seed << endl;
-	std::mt19937_64 rng(args.seed);
-	std::uniform_int_distribution<uint64_t> distribution(0, text_buf.size()-args.pat_len);
-	auto dice = bind(distribution, rng);
-	
-	S ngram(args.pat_len,'0');
-	
-	if (args.min_sampling) {
-		string_vector_hash_fn h;
-		unordered_map<size_t, S> ngram_storage(args.ngram_samples << 1);
-		unordered_map<size_t, size_t> ngram_counts(args.ngram_samples << 1);
-		for (int i = args.ngram_samples; i--;) {
-			get_ngram(text_buf, dice, args.pat_len, ngram);
-			ngram_counts[h(ngram)]++;
-			ngram_storage[h(ngram)] = ngram;
-		}
-		
-		vector<S> ngrams;
-		ngrams.reserve(args.pat_cnt);
-		auto threshold = args.ngram_samples / args.min_sampling;
-		for (const auto& it : ngram_counts)
-			if (it.second >= threshold)
-				ngrams.push_back(ngram_storage[it.first]);
-			std::shuffle(ngrams.begin(), ngrams.end(), rng);
-		
-		if (args.pat_cnt > ngrams.size()) {
-			std::cerr << "Not enough ngrams found" << endl;
-			exit(1);
-		}
-		for (size_t i = 0; i < args.pat_cnt; ++i)
-			for(size_t j = 0; j < ngrams[i].size(); ++j)
-				cout << ngrams[i][j] << ((j+1 == ngrams[i].size()) ? "\n" : (args.int_collection ? " " : ""));
-	} else {
-		string_vector_hash_fn h; vector<S> results;
-		unordered_map<size_t,bool> vis;
-		while (results.size() < args.pat_cnt) {
-			get_ngram(text_buf, dice, args.pat_len, ngram);
-			if(!vis[h(ngram)]) {
-				results.push_back(ngram); vis[h(ngram)] = true;
-			}
-		}
-		for (const S& x: results)
-			for(size_t j = 0; j < x.size(); ++j)
-				cout << x[j] << ((j+1 == x.size()) ? "\n" : (args.int_collection ? " " : ""));
-	}
-	
-	return EXIT_SUCCESS;
+    if (text_buf.size() < args.pat_len) {
+        std::cout << "ERROR: text.size()=" << text_buf.size() << " < m=" << args.pat_len << std::endl;
+        return 1;
+    }
+
+    //cout << "seed = " << args.seed << endl;
+    std::mt19937_64 rng(args.seed);
+    std::uniform_int_distribution<uint64_t> distribution(0, text_buf.size() - args.pat_len);
+    auto dice = bind(distribution, rng);
+
+    S ngram(args.pat_len, '0');
+
+    if (args.min_sampling) {
+        string_vector_hash_fn h;
+        unordered_map<size_t, S> ngram_storage(args.ngram_samples << 1);
+        unordered_map<size_t, size_t> ngram_counts(args.ngram_samples << 1);
+        for (int i = args.ngram_samples; i--;) {
+            get_ngram(text_buf, dice, args.pat_len, ngram);
+            ngram_counts[h(ngram)]++;
+            ngram_storage[h(ngram)] = ngram;
+        }
+
+        vector<S> ngrams;
+        ngrams.reserve(args.pat_cnt);
+        auto threshold = args.ngram_samples / args.min_sampling;
+        for (const auto & it : ngram_counts)
+            if (it.second >= threshold)
+                ngrams.push_back(ngram_storage[it.first]);
+        std::shuffle(ngrams.begin(), ngrams.end(), rng);
+
+        if (args.pat_cnt > ngrams.size()) {
+            std::cerr << "Not enough ngrams found" << endl;
+            exit(1);
+        }
+        for (size_t i = 0; i < args.pat_cnt; ++i)
+            for (size_t j = 0; j < ngrams[i].size(); ++j)
+                cout << ngrams[i][j] << ((j + 1 == ngrams[i].size()) ? "\n" : (args.int_collection ? " " : ""));
+    } else {
+        string_vector_hash_fn h; vector<S> results;
+        unordered_map<size_t, bool> vis;
+        while (results.size() < args.pat_cnt) {
+            get_ngram(text_buf, dice, args.pat_len, ngram);
+            if (!vis[h(ngram)]) {
+                results.push_back(ngram); vis[h(ngram)] = true;
+            }
+        }
+        for (const S & x : results)
+            for (size_t j = 0; j < x.size(); ++j)
+                cout << x[j] << ((j + 1 == x.size()) ? "\n" : (args.int_collection ? " " : ""));
+    }
+
+    return EXIT_SUCCESS;
 }
 
-int main(int argc,char* const argv[])
+int main(int argc, char* const argv[])
 {
     /* parse command line */
-    cmdargs_t args = parse_args(argc,argv);
+    cmdargs_t args = parse_args(argc, argv);
     if (args.int_collection) {
-		std::string text_file = args.collection_dir+"/"+surf::TEXT_FILENAME;
-		sdsl::int_vector_buffer<> text_buf(text_file, std::ios::in);
-		return find_ngrams<vector<int>>(text_buf, args);
+        std::string text_file = args.collection_dir + "/" + surf::TEXT_FILENAME;
+        sdsl::int_vector_buffer<> text_buf(text_file, std::ios::in);
+        return find_ngrams<vector<int>>(text_buf, args);
     }
     else {
-		std::string text_file = args.collection_dir+"/"+surf::TEXT_FILENAME_BYTE;
-		sdsl::int_vector_buffer<8> text_buf(text_file, std::ios::in);
-		return find_ngrams<string>(text_buf, args);
-	}
+        std::string text_file = args.collection_dir + "/" + surf::TEXT_FILENAME_BYTE;
+        sdsl::int_vector_buffer<8> text_buf(text_file, std::ios::in);
+        return find_ngrams<string>(text_buf, args);
+    }
 
 }
 

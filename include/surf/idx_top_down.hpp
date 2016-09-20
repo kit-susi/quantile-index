9ba18198ea3a109fdb3d59dc757d24f8b6d1ae6a
Julian Labeit <julianlabeit@gmail.com>
2016-09-20 21:47:08 +0200

    Construct basic data structures of idx_top_down.

--- include/surf/idx_top_down.hpp
+++ include/surf/idx_top_down.hpp
@@ -75,10 +75,46 @@ class idx_top_down
     }
 };
 
-template<typename t_csa>
-void construct(idx_top_down<t_csa>& idx,
+template<typename t_csa,
+         typename t_border,
+         typename t_border_rank,
+         typename t_border_select>
+void construct(idx_top_down<t_csa,
+               t_border, t_border_rank,
+               t_border_select>& idx,
                const std::string&, sdsl::cache_config& cc, uint8_t num_bytes) {
-        std::cerr << "Not implemented yet" << std::endl;
-        abort();
+    using t_wtd = WTD_TYPE;
+    cout << "...CSA" << endl; // CSA to get the lex. range
+    if (!cache_file_exists<t_csa>(surf::KEY_CSA, cc))
+    {
+        t_csa csa;
+        construct(csa, "", cc, 0);
+        store_to_cache(csa, surf::KEY_CSA, cc, true);
+    }
+    cout << "...WTD" << endl;
+    // Document array and wavelet tree of it
+    // Note: This also constructs doc borders.
+    if (!cache_file_exists<t_wtd>(surf::KEY_WTD, cc)) {
+        construct_darray<t_csa::alphabet_type::int_width>(cc, false);
+        t_wtd wtd;
+        construct(wtd, cache_file_name(surf::KEY_DARRAY, cc), cc);
+        cout << "wtd.size() = " << wtd.size() << endl;
+        cout << "wtd.sigma = " << wtd.sigma << endl;
+        store_to_cache(wtd, surf::KEY_WTD, cc, true);
+    }
+    cout << "...DOC_BORDER" << endl;
+    if (!cache_file_exists<t_border>(surf::KEY_DOCBORDER, cc) or
+            !cache_file_exists<t_border_rank>(surf::KEY_DOCBORDER_RANK, cc) or
+            !cache_file_exists<t_border_select>(surf::KEY_DOCBORDER_SELECT, cc))
+    {
+        bit_vector doc_border;
+        load_from_cache(doc_border, surf::KEY_DOCBORDER, cc);
+        t_border sd_doc_border(doc_border);
+        store_to_cache(sd_doc_border, surf::KEY_DOCBORDER, cc, true);
+        t_border_rank doc_border_rank(&sd_doc_border);
+        store_to_cache(doc_border_rank, surf::KEY_DOCBORDER_RANK, cc, true);
+        t_border_select doc_border_select(&sd_doc_border);
+        store_to_cache(doc_border_select, surf::KEY_DOCBORDER_SELECT, cc, true);
+    }
 }
 } // namespace surf

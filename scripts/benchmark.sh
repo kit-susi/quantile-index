4441777926d898a792ef1f6454554ac7e663bb78
Julian Labeit <julianlabeit@gmail.com>
2016-12-20 22:14:05 +0100

    Changed benchmark script.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -xe
-TXT_CONFIGS="IDX_NN IDX_NN_DOCID_SMART IDX_NN_K3_DAAT"
+TXT_CONFIGS="IDX_NN IDX_NN_QUANTILE IDX_NN_QUANTILE_LG"
 INT_CONFIGS="IDX_NN_INT IDX_NN_DOCID_SMART_INT IDX_NN_K3_DAAT_INT"
 
-scripts/build_config.sh $TXT_CONFIGS $INT_CONFIGS
+scripts/build_config.sh $TXT_CONFIGS #$INT_CONFIGS
 scripts/build.sh gen_patterns
 
-coll=collections/TEST_TXT
+coll=collections/ENWIKISML
 if [[ $# > 0 ]]; then
     coll="$1"
 fi
 
 scripts/compare.py -c "$coll" $TXT_CONFIGS \
-    -b build/release -q 3000 -r 1
-scripts/compare.py -c "$coll" $INT_CONFIGS \
-    -b build/release -q 3000 -r 1
+    -b build/release -q 3000 -r 1 --no_multi_occ
+#scripts/compare.py -c "$coll" $INT_CONFIGS \
+    #-b build/release -q 3000 -r 1

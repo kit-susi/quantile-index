41a899a4d810ab289ffe1f0a271f1d7591dc13e2
Julian Labeit <julianlabeit@gmail.com>
2017-01-09 13:46:27 +0100

    Build indizes partly in parallel.

--- scripts/buildIndexes.py
+++ scripts/buildIndexes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import os
 import argparse
+from multiprocessing import Process
 
 sampling = [4, 8, 16, 32, 64]
 quantiles = [8, 16, 32, 64, 128]
@@ -13,6 +14,12 @@ def build_executable(s, q):
 def build_index(s, q, c, build_dir):
     print 'Building index with sampling %d and quantile %d' % (s,q)
     os.system('./%s/surf_index-%s -c %s' % (build_dir, config(s, q), c))
+
+def run_parallel(processes):
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
@@ -30,12 +37,15 @@ if __name__ == '__main__':
                 build_executable(s, q)
 
     print "Done building all executables"
-    for s in sampling:
-        for q in quantiles:
-            build_index(s, q, args.collection, args.build_dir)
-    configs = [config(s,q) for s in sampling for q in quantiles]
+    # Build first index.
+    build_index(sampling[0], quantiles[0], args.collection, args.build_dir)
+    # Build all samplings in parallel.
+    run_parallel([Process(target=build_index, args=(s, quantiles[0], args.collection, args.build_dir)) for s in sampling])
+    # Build all quantiles in parallel.
+    run_parallel([Process(target=build_index, args=(sampling[0], q, args.collection, args.build_dir)) for q in quantiles])
 
     print "Comparing indizes"
+    configs = [config(s,q) for s in sampling for q in quantiles]
     command = './scripts/compare.py %s -c %s -b %s --no_multi_occ -q 10000 -k 10 -n 4  -r 1' % (' '.join(configs), args.collection, args.build_dir)
     print "Running command: ", command
     os.system(command)

dc0aa59d18bf3ae63d00a1b27c90c8432027733c
Julian Labeit <julianlabeit@gmail.com>
2017-01-09 14:35:26 +0100

    Added more functionality.

--- scripts/buildIndexes.py
+++ scripts/buildIndexes.py
@@ -2,15 +2,22 @@
 import os
 import argparse
 from multiprocessing import Process
+import subprocess
 
 sampling = [4, 8, 16, 32, 64]
 quantiles = [8, 16, 32, 64, 128]
 
+output_file = open('result_file', "w")
+def output(s):
+    output_file.write(s + "\n")
+    output_file.flush()
+
 def config(s,q):
     return 'IDX_NN_QUANTILE_LG_%d_%d' % (s, q)
-def build_executable(s, q):
-    os.system('./scripts/build_config.sh -d %s' % config(s, q))
-    os.system('./scripts/build_config.sh %s' % config(s, q))
+
+def build_executable(configs):
+    os.system('./scripts/build_config.sh -d %s' % ' '.join(configs))
+    os.system('./scripts/build_config.sh %s' % ' '.join(configs))
 
 def build_index(s, q, c, build_dir):
     print 'Building index with sampling %d and quantile %d' % (s,q)
@@ -22,6 +29,21 @@ def run_parallel(processes):
     for p in processes:
         p.join()
 
+def lastLine(s):
+    return s.split('\n')[-2]
+
+def printIndexSizes(configs, collection, build_dir):
+    for config in configs:
+    	proc = subprocess.Popen(['./%s/surf_index-%s' % (build_dir, config), "-c", collection, "-m m"], stdout=subprocess.PIPE)
+    	out = proc.communicate()[0]
+    	output(config + ", " + lastLine(out.upper()))
+
+def printIndexSpeed(configs, collection, build_dir):
+    command = ['./scripts/compare.py'] + configs + ["-c", collection, "-b", build_dir, '--no_multi_occ', '-q 10000', '-k 10', '-n 4', '-r 1']
+    print command
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output(proc.communicate()[0])
+
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('-c', dest='collection', required=True, metavar='DIRECTORY',
@@ -32,10 +54,9 @@ if __name__ == '__main__':
             help='Rebuilds all executables')
     args = p.parse_args()
  
+    configs = [config(s,q) for s in sampling for q in quantiles]
     if args.rebuild:
-        for s in sampling:
-            for q in quantiles:
-                build_executable(s, q)
+	build_executable(configs)
 
     print "Done building all executables"
     # Build first index.
@@ -45,8 +66,9 @@ if __name__ == '__main__':
     # Build all quantiles in parallel.
     run_parallel([Process(target=build_index, args=(sampling[0], q, args.collection, args.build_dir)) for q in quantiles])
 
+    print "Index sizes" 
+    printIndexSizes(configs, args.collection, args.build_dir)
+
     print "Comparing indizes"
-    configs = [config(s,q) for s in sampling for q in quantiles]
-    command = './scripts/compare.py %s -c %s -b %s --no_multi_occ -q 10000 -k 10 -n 4  -r 1' % (' '.join(configs), args.collection, args.build_dir)
-    print "Running command: ", command
-    os.system(command)
+    printIndexSpeed(configs, args.collection, args.build_dir)
+

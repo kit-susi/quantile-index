dcbbc9de14136a8363d9ce7ba859049ad1d6b5d1
Julian Labeit <julianlabeit@gmail.com>
2017-01-25 00:26:48 +0100

    Made ticks better.

--- experiments/exp1/plot.R
+++ experiments/exp1/plot.R
@@ -8,11 +8,11 @@ for (col in collections) {
     max = data[data$collection==col & data$q == 1,]$G
     data[data$collection==col,]$G <- 100 * data[data$collection==col,]$G / max
 }
-yticks = c(1,2,5,10,25,50,100)
 pp <- ggplot(data, aes(q, G,color=collection))
 pp <- pp + geom_line() 
 pp <- pp + geom_point()
-pp <- pp + scale_y_log10()
-#pp <- pp + scale_y_log10(breaks=yticks)
+#pp <- pp + scale_y_continuous()
+pp <- pp + scale_y_log10(breaks=c(1,2,10,25,50,100))
+pp <- pp + scale_x_continuous(breaks=c(1,16,32,64,128,256))
 plot(pp)
 dev.off()
